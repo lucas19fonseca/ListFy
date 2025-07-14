911d43d5968ab64d9890da77b85b2b2c570623e5
lucas19fonseca <lucas19fonseca@gmail.com>
2025-07-13 23:47:36 -0300

    navbar finalizada

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,18 +1,32 @@
+
 export default function Todo() {
   return (
     <div className="grid grid-cols-[250px_1fr] h-screen">
       {/* Sidebar */}
-      <div className="flex flex-col bg-amber-900 text-white p-6">
-        <h2 className="text-2xl font-bold mb-8">Listify</h2>
+      <div className="flex flex-col bg-[#3A3A3A] text-white p-6 gap-2.5 shadow-2xl">
+        <h2 className="text-2xl font-bold mb-8"><i class="fa-solid fa-clipboard"></i> Listify</h2>
         <nav className="flex flex-col gap-4">
-          <button className="text-left hover:text-amber-300">Todas as Tarefas</button>
-          <button className="text-left hover:text-amber-300">Importantes</button>
-          <button className="text-left hover:text-amber-300">Concluídas</button>
+          <button className="text-left hover:opacity-65 transition duration-300"><i class="fa-solid fa-pencil fa-xs"></i> Criar nova lista</button>
+          <button className="text-left hover:opacity-65 transition duration-300"><i class="fa-solid fa-tasks"></i>  Buscar Listas</button>
+          <button className="text-left hover:opacity-65 transition duration-300"><i class="fa-solid fa-clock"></i>  Incompletas</button>
+          <button className="text-left hover:opacity-65 transition duration-300"><i class="fa-solid fa-check"></i>  Concluídas</button>
+          <button className="text-left hover:opacity-65 transition duration-300"><i class="fa-solid fa-exclamation-triangle"></i>  Importantes</button>
+        </nav>
+
+        <div className="flex flex-col bg-[#3A3A3A] text-white pt-15">
+        <span className="text-[17px] font-light opacity-50 mb-3">Listas</span>
+        <nav className="flex flex-col gap-4">
+          <button className="text-left hover:text-amber-300 ">Exemplo1</button>
+          <button className="text-left hover:text-amber-300">Exemplo1</button>
+          <button className="text-left hover:text-amber-300">Exemplo1</button>
+          <button className="text-left hover:text-amber-300">Exemplo1</button>
         </nav>
       </div>
+      </div>
+      
 
       {/* Área principal */}
-      <div className="bg-[#f4f4f4] p-8">
+      <div className="bg-[#5e5e5e5e] p-8">
         <h1 className="text-4xl font-bold mb-6">Tarefas</h1>
 
         {/* Adicionar tarefa */}
